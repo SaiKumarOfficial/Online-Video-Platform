ae9a403599380722ea60f29e7f8855ca3554a7d0
Sai Kumar <95096218+SaiKumarOfficial@users.noreply.github.com>
2023-07-29 12:10:15 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MLproject-base-template
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup,find_packages
+from typing import List   
+
+def get_requiremnets() -> List[str]:
+    file = open("requirements.txt","r")
+  
+
+    req_list:List[str] = []
+    
+    return req_list
+    """to install for custom code and generate libraries we should write  this setup.py file """
+
+setup(
+    name="<project-name>",
+    version="0.0.1",
+    author="saikumar",
+    author_email="sai680513@gmail.com",
+    packages = find_packages()   
+)
